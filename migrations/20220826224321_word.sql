21554e94b8c01f1e8889f6116237c5ee4f03290b
ta715 <moeka20011023@gmail.com>
2022-08-29 11:13:00 +0900

    FE2022-69 [fix] migrationの修正

--- migrations/20220826224321_word.sql
+++ migrations/20220826224321_word.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS words (
-    id char(36) AUTO_INCREMENT,
+    id char(36) NOT NULL,
     user_id char(36) NOT NULL,
-    word_name varchar(30) NOT NULL,
+    word_name varchar(50) NOT NULL,
     meaning varchar(255) NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
